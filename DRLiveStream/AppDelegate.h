55fb7d1d80f7b57559aad48cb078ab3663b0f8c3
xqzh <xqzh@ideabinder.com>
2016-09-01 14:32:35 +0800

    Initial Commit

---
+++ DRLiveStream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRLiveStream
+//
+//  Created by xqzh on 16/9/1.
+//  Copyright © 2016年 xqzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DRLiveStream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DRLiveStream
+//
+//  Created by xqzh on 16/9/1.
+//  Copyright © 2016年 xqzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
